838a987b09df160e320eb976a7c6ea26baa90afe
liyuantao <2294515310@qq.com>
2024-03-18 10:07:46 +0800

    idea git commit03

--- git-demo/src/main/java/cn/tedu/Main.java
+++ git-demo/src/main/java/cn/tedu/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!222222222222222222");
+        System.out.println("Hello world!23333333333333333333");
+
+
     }
 }
